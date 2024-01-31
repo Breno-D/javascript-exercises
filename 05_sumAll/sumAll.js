336eba5ae4f039b40e0b0d6e7f5cb6346e49d0f6
Breno Dória <brenord@hotmail.com.br>
2024-01-31 14:02:31 -0300

    Ex 5 Solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let numberToReturn = 0;
+    if(!(typeof num1 === 'number') || !(typeof num2 === 'number') || num1<0 || num2<0)
+    {
+        return "ERROR";
+    }
+    else if(num1>num2)
+    {
+        for(num2; num2<=num1;num2++)
+        {
+            numberToReturn += num2;
+        }
+    }
+    else
+    {
+        for(num1; num1<=num2;num1++)
+        {
+            numberToReturn += num1;
+        }
+    }
+    return numberToReturn;
 };
 
 // Do not edit below this line
