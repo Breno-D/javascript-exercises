1d4c271988b40ef488890625d130450face6807d
Breno Dória <brenord@hotmail.com.br>
2024-01-30 21:56:48 -0300

    Ex3 Solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(stringToBeReversed) {
+    reversedString = "";
+    for(let i=stringToBeReversed.length-1; i>=0;i--)
+    {
+        reversedString += stringToBeReversed[i];
+    }
 
+    return reversedString;
 };
 
 // Do not edit below this line
