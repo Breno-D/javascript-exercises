0db0b8b0d4103dc10fb137e2342e9e22246838f8
Breno Dória <brenord@hotmail.com.br>
2024-01-30 22:13:18 -0300

    Ex 4 Solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayReceived, ...removedObjects) {
+    let arrayToReturn = [] 
+    
+    arrayReceived.forEach((element) => 
+    {
+        if(!removedObjects.includes(element))
+        {
+            arrayToReturn.push(element)
+        }
+    });
+    return arrayToReturn;
 };
 
 // Do not edit below this line
