72cbadaa526204a01b9814b818bce91899e14902
Breno Dória <brenord@hotmail.com.br>
2024-01-30 21:50:51 -0300

    Ex2 Solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(repeatedString, numberOfRepetitions) {
+    let stringToReturn = "";
+    for(let i=0; i<numberOfRepetitions; i++)
+    {
+        stringToReturn += repeatedString;
+    }
 
+    if(numberOfRepetitions<0)
+    {
+        return "ERROR";
+    }
+    else
+    {
+        return stringToReturn;
+    }
 };
 
+
+ 
 // Do not edit below this line
 module.exports = repeatString;
