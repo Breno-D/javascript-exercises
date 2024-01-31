f6b1014f57689a04100c411a6668d4b0705ff626
Breno Dória <brenord@hotmail.com.br>
2024-01-31 14:13:25 -0300

    Ex 7 Solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFarenheit) {
+  let tempInCelsiusToReturn = (tempInFarenheit - 32)*(5/9);
+  tempInCelsiusToReturn =  Math.round(tempInCelsiusToReturn * 10) / 10;
+  return tempInCelsiusToReturn;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  let tempInFarenheitToReturn = (tempInCelsius*(9/5)+32);
+  tempInFarenheitToReturn = Math.round(tempInFarenheitToReturn * 10) / 10;
+  return tempInFarenheitToReturn;
 };
 
 // Do not edit below this line
