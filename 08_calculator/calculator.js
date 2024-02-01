c6bb43a2b783abc3a0e2f379937d3a3451d59253
Breno Dória <brenord@hotmail.com.br>
2024-01-31 22:23:24 -0300

    Ex 8 Solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return sum([num1, num2]);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return sum([num1, -num2]);
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  return args.reduce((accumulator,value) => accumulator + value,0);
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return args.reduce((accumulator,value) => accumulator * value,1);
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return Math.pow(number, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(factorialNumber) {
+  let numbersToMultiply = [1]
+	for(factorialNumber; factorialNumber>1;factorialNumber--)
+  {
+    numbersToMultiply.push(factorialNumber);
+  }
+  return multiply(numbersToMultiply);
 };
 
 // Do not edit below this line
